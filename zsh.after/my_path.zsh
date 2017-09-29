6f2b248c4ffcc7c98825921c137b294bdc604e18
daewon <daewon@apache.org>
2017-09-29 15:51:19 +0900

    add

--- zsh.after/my_path.zsh
+++ zsh.after/my_path.zsh
@@ -2,3 +2,5 @@ export PATH=$PATH:$HOME/apps/bin
 HISTSIZE=10000000000000
 SAVEHIST=10000000000000
 
+# Add Visual Studio Code (code)
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
